1c1775fd0638791f9802944175953ea1f6748126
Takashi <t.ogura@gmail.com>
2016-01-26 23:59:29 +0900

    Add required packages to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(name='jsp', version='0.0.1', description='json pub/sub using zmq',
       author='Takashi Ogura', author_email='t.ogura@gmail.com',
       url='http://github.com/OTL/jsp', packages=find_packages(),
+      install_requires=[
+        'zmq',
+        'tornado'
+        ],
       entry_points= {
         'console_scripts': [
             'jps_forwarder = jps.forwarder:main',
