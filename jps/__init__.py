21bf1fa84aa582bf519b6e1a961ac625711b7c9a
Takashi <t.ogura@gmail.com>
2016-01-30 12:44:46 +0900

    Fix import for python3

--- jps/__init__.py
+++ jps/__init__.py
@@ -1,6 +1,6 @@
-from publisher import Publisher
-from subscriber import Subscriber
-import forwarder
-import tools
+from .publisher import Publisher
+from .subscriber import Subscriber
+from . import forwarder
+from . import tools
 
 __all__ = ["Publisher", "Subscriber", "forwarder", "tools"]
