5b93660765b34c3850528b04cb14672f66ff2e15
Takashi Ogura <t.ogura@gmail.com>
2016-03-08 04:01:58 +0900

    Use importlib

--- jps/__init__.py
+++ jps/__init__.py
@@ -7,6 +7,7 @@ from .common import DEFAULT_HOST
 from . import forwarder
 from . import tools
 from . import utils
+from . import launcher
 
-__all__ = ['Publisher', 'Subscriber', 'ArgumentParser', 'forwarder', 'utils',
+__all__ = ['Publisher', 'Subscriber', 'ArgumentParser', 'forwarder', 'utils', 'launcher',
            'tools', 'DEFAULT_PUB_PORT', 'DEFAULT_SUB_PORT', 'DEFAULT_HOST']
--- jps/launcher.py
+++ jps/launcher.py
@@ -1,11 +1,11 @@
 from multiprocessing import Process
-
+import importlib
 
 def launch_modules(module_names):
     '''launch module.main functions in another process'''
     processes = []
     for module_name in module_names:
-        m = __import__(module_name)
+        m = importlib.import_module(module_name)
         p1 = Process(target=m.main)
         p1.start()
         processes.append(p1)
