b0ad71268410f2a6bdf044d9bf70c9d344abfde0
Takashi Ogura <t.ogura@gmail.com>
2016-06-08 17:31:12 +0900

    Change Bridge behavior to stop suport bidirectional
    Now it supports only oneway bridges.

--- jps/bridge.py
+++ jps/bridge.py
@@ -3,47 +3,53 @@ import functools
 from .publisher import Publisher
 from .subscriber import Subscriber
 
-class SingleTopicBridge(object):
-    def __init__(self, topic, remote_added_suffix, remote_host=None,
-                 remote_pub_port=None, remote_sub_port=None):
-        self._local_publisher = Publisher(topic + remote_added_suffix)
+
+class UploadSingleTopicBridge(object):
+    def __init__(self, topic, remote_host=None, remote_pub_port=None):
         self._local_subscriber = Subscriber(topic, self.local_to_remote)
-        self._remote_publisher = Publisher(topic + remote_added_suffix,
-                                           host=remote_host,
+        self._remote_publisher = Publisher(topic, host=remote_host,
                                            pub_port=remote_pub_port)
+
+    def spin(self):
+        self._local_subscriber.spin(use_thread=True)
+
+    def local_to_remote(self, msg):
+        self._remote_publisher.publish(msg)
+
+
+class DownloadSingleTopicBridge(object):
+    def __init__(self, topic, remote_host=None, remote_sub_port=None):
+        self._local_publisher = Publisher(topic)
         self._remote_subscriber = Subscriber(topic, self.remote_to_local,
                                              host=remote_host,
                                              sub_port=remote_sub_port)
 
     def spin(self):
-        self._local_subscriber.spin(use_thread=True)
         self._remote_subscriber.spin(use_thread=True)
-
-    def local_to_remote(self, msg):
-        self._remote_publisher.publish(msg)
 
     def remote_to_local(self, msg):
         self._local_publisher.publish(msg)
 
 
 class Bridge(object):
-    def __init__(self, target_topic_names, suffix, remote_host=None,
+    def __init__(self, upload_topic_names, download_topic_names, remote_host=None,
                  remote_pub_port=None, remote_sub_port=None):
         '''
         Pub/Sub in different jps network
-        Topic names are renamed between remote networks.
-        This is to avoid infinity loop.
 
-        topic_a        (local) ---> topic_a.suffix (remote)
-        topic_a.suffix (local) <--- topic_a (remote)
+        upload_topic_names and download_topic_names should not 
+        contain same names. It causes infinity loop.
         '''
-        if suffix == '':
-            raise Exception('empty suffix causes infinity loop')
+        if len(set(set(upload_topic_names) & set(download_topic_names))) > 0:
+            raise Exception('upload_topic_names and download_topic_names should not' + 
+                            'contain same names')
         self._bridges = []
-        for topic in target_topic_names:
-            self._bridges.append(SingleTopicBridge(
-                topic, suffix, remote_host, remote_pub_port=remote_pub_port,
-                remote_sub_port=remote_sub_port))
+        for topic in upload_topic_names:
+            self._bridges.append(UploadSingleTopicBridge(
+                topic, remote_host, remote_pub_port=remote_pub_port))
+        for topic in download_topic_names:
+            self._bridges.append(DownloadSingleTopicBridge(
+                topic, remote_host, remote_sub_port=remote_sub_port))
 
     def spin(self):
         for b in self._bridges:
--- test/test_bridge.py
+++ test/test_bridge.py
@@ -19,33 +19,43 @@ def test_bridge():
         target=jps.forwarder.main, args=(55322, 55323))
     forwarder_process.start()
     time.sleep(0.1)
-    b = jps.Bridge(('bridge1', 'bridge2', 'bridge3', 'bridge4'),
-                   '.remote',
+    b = jps.Bridge(('bridge1', 'bridge2'), ('bridge3', 'bridge4'),
                    remote_pub_port=55322, remote_sub_port=55323)
     b.spin()
     # local to remote
     lp1 = jps.Publisher('bridge1')
     lp2 = jps.Publisher('bridge2')
     time.sleep(0.1)
-    rs1 = jps.Subscriber('bridge1.remote', sub_port=55323)
-    rs2 = jps.Subscriber('bridge2.remote', sub_port=55323)
+    rs1 = jps.Subscriber('bridge1', sub_port=55323)
+    rs2 = jps.Subscriber('bridge2', sub_port=55323)
+    h3 = MessageHolder()
+    rs3 = jps.Subscriber('bridge3', h3, sub_port=55323)
+    
     time.sleep(0.1)
     lp1.publish('hoge1')
     lp2.publish('hoge2')
     assert rs1.next() == 'hoge1'
     assert rs2.next() == 'hoge2'
+    rs3.spin_once()
+    assert len(h3.saved_msg) == 0
 
     # remote to local
-    rp1 = jps.Publisher('bridge3', pub_port=55322)
-    rp2 = jps.Publisher('bridge4', pub_port=55322)
+    rp1 = jps.Publisher('bridge1', pub_port=55322)
+    rp3 = jps.Publisher('bridge3', pub_port=55322)
+    rp4 = jps.Publisher('bridge4', pub_port=55322)
     time.sleep(0.1)
-    ls1 = jps.Subscriber('bridge3.remote')
-    ls2 = jps.Subscriber('bridge4.remote')
+    h1 = MessageHolder()
+    ls1 = jps.Subscriber('bridge1', h1)
+    ls3 = jps.Subscriber('bridge3')
+    ls4 = jps.Subscriber('bridge4')
     time.sleep(0.1)
-    rp1.publish('hoge3')
-    rp2.publish('hoge4')
-    assert ls1.next() == 'hoge3'
-    assert ls2.next() == 'hoge4'
-
+    rp1.publish('hoge1')
+    rp3.publish('hoge3')
+    rp4.publish('hoge4')
+    assert ls3.next() == 'hoge3'
+    assert ls4.next() == 'hoge4'
+    ls1.spin_once()
+    assert len(h1.saved_msg) == 0
+    
     os.kill(forwarder_process.pid, signal.SIGINT)
     forwarder_process.join(1.0)
