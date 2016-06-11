3e56f467732cfe52a75e0ef528f7a50b2eaadaf1
Takashi Ogura <t.ogura@gmail.com>
2016-06-11 19:05:51 +0900

    version 0.1.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class PyTest(TestCommand):
 
 
 setup(name='jps',
-      version='0.0.2a1',
+      version='0.1.0',
       description='json pub/sub using zmq',
       author='Takashi Ogura',
       author_email='t.ogura@gmail.com',
